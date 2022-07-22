9c3944c773c910128302c0a1ed0e15ab0a7285af
Jared <landetta@icloud.com>
2022-07-21 19:55:00 -0600

    Initial Commit

---
+++ GithubTest/GithubTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubTestApp.swift
+//  GithubTest
+//
+//  Created by Jared  Landetta  on 7/21/22.
+//
+
+import SwiftUI
+
+@main
+struct GithubTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
